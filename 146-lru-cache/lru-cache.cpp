d63ab4d973de69646c6d3ada7ca7bdf09e1d3f6a
Breny Moirangthem <brenymoirangthem@gmail.com>
2026-09-25 19:44:23 +0530

    Time: 19 ms (67.23%) | Memory: 49.1 MB (62.59%) - LeetSync

---
+++ 146-lru-cache/lru-cache.cpp
@@ -0,0 +1,78 @@
+class LRUCache {
+private:
+    int capacity;
+
+    struct Node {
+        int key;
+        int value;
+        Node* prev;
+        Node* next;
+
+        Node(int k, int v) {
+            key = k;
+            value = v;
+            prev = nullptr;
+            next = nullptr;
+        }
+    };
+
+    std::unordered_map<int, Node*> cache;
+    Node* head;
+    Node* tail;
+
+    void remove(Node* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void insert(Node* node) {
+        node->next = head->next;
+        node->prev = head;
+
+        head->next->prev = node;
+        head->next = node;
+    }
+
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    int get(int key) {
+        if (cache.find(key) == cache.end()) {
+            return -1;
+        }
+
+        Node* node = cache[key];
+
+        remove(node);
+        insert(node);
+
+        return node->value;
+    }
+
+    void put(int key, int value) {
+        if (cache.find(key) != cache.end()) {
+            remove(cache[key]);
+            delete cache[key];
+        }
+
+        Node* node = new Node(key, value);
+        cache[key] = node;
+        insert(node);
+
+        if (cache.size() > capacity) {
+            Node* lru = tail->prev;
+
+            remove(lru);
+            cache.erase(lru->key);
+            delete lru;
+        }
+    }
+};
